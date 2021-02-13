5bbf6dae6cc69b0b1f72c6643d22bb210dfc0f54
Ankit Giri <angiri@microsoft.com>
2021-02-13 18:30:56 +0530

    ection 5: Spring Dependency Injection - XML

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -1,9 +1,22 @@
 package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
-
+	
+		private FortuneService fortuneService;
+		
+		//Constructor dependency injection
+		public BaseballCoach(FortuneService theFortuneService) {
+			this.fortuneService = theFortuneService;
+		}
+	
 		@Override
 		public String getDailyWorkout() {
 			return "Spend 30 minutes on batting practices";
+		}
+
+		@Override
+		public String getDailyFortune() {
+
+			return fortuneService.getFortune();
 		}
 }
